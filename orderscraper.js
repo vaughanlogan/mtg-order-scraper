52e0a57a40915f460b5d5f32ecee254b29dbcadc
Vaughan Logan <vlogan1@babson.edu>
2025-03-02 12:42:54 -0500

    Set name reassignment wip

--- orderscraper.js
+++ orderscraper.js
@@ -1,45 +1,83 @@
+
 function jsonToCsv(jsonData) {
-    let csv = '';
-    
-    // Extract headers
-    const headers = Object.keys(jsonData[0]);
-    csv += headers.join(',') + '\n';
-    
-    // Extract values
-    jsonData.forEach(obj => {
-        const values = headers.map(header => obj[header]);
-        csv += values.join(',') + '\n';
-    });
-    
-    return csv;
+  let csv = "";
+
+  // Extract headers
+  const headers = Object.keys(jsonData[0]);
+  csv += headers.join(",") + "\n";
+
+  // Extract values
+  jsonData.forEach((obj) => {
+    const values = headers.map((header) => obj[header]);
+    csv += values.join(",") + "\n";
+  });
+
+  return csv;
 }
 
-const raw_orders = document.getElementsByClassName("orderWrap");
-console.log("RAW ORDERS:");
-console.log(raw_orders);
+function scrapeOrders() {
+  let orders = [];
 
-var orders = [];
+  //extract raw orders
+  const raw_orders = document.getElementsByClassName("orderWrap");
+  console.log("RAW ORDERS:");
+  console.log(raw_orders);
 
-for (const x of raw_orders) {
+  //create json from raw orders
+  for (const x of raw_orders) {
     orders.push({
-        date: new Date(x.querySelector('[data-aid="spn-sellerorderwidget-orderdate"]').innerText),
-        id: x.getElementsByClassName("orderHeader")[0].children[2].innerText.substring(13),
-        items: Array.from(x.getElementsByClassName("orderTable")[0].querySelectorAll(".trOdd,.trEven")).map((i)=> new Object({
-            Count: i.getElementsByClassName("orderHistoryQuantity")[0].innerText,
-            Name: i.getElementsByClassName("orderHistoryItems")[0].getElementsByTagName("a")[0].innerText,
-            Edition: i.getElementsByClassName("orderHistoryItems")[0].getElementsByTagName("span")[0].lastChild.textContent.substring(2).trimEnd(),
-            Condition: i.getElementsByClassName("orderHistoryDetail")[0].innerText.split("Condition: ")[1].split(" Foil")[0],
-            Foil: i.getElementsByClassName("orderHistoryDetail")[0].innerText.split("Condition: ")[1].split(" Foil").length==2,
-            "Purchase Price": parseFloat(i.getElementsByClassName("orderHistoryPrice")[0].innerText.substring(1))
-        }))
+      date: new Date(
+        x.querySelector(
+          '[data-aid="spn-sellerorderwidget-orderdate"]'
+        ).innerText
+      ),
+      id: x
+        .getElementsByClassName("orderHeader")[0]
+        .children[2].innerText.substring(13),
+      items: Array.from(
+        x
+          .getElementsByClassName("orderTable")[0]
+          .querySelectorAll(".trOdd,.trEven")
+      ).map(
+        (i) =>
+          new Object({
+            Count: i.getElementsByClassName("orderHistoryQuantity")[0]
+              .innerText,
+            Name: i
+              .getElementsByClassName("orderHistoryItems")[0]
+              .getElementsByTagName("a")[0].innerText,
+            Edition: i
+              .getElementsByClassName("orderHistoryItems")[0]
+              .getElementsByTagName("span")[0]
+              .lastChild.textContent.substring(2)
+              .trimEnd(),
+            Condition: i
+              .getElementsByClassName("orderHistoryDetail")[0]
+              .innerText.split("Condition: ")[1]
+              .split(" Foil")[0],
+            Foil:
+              i
+                .getElementsByClassName("orderHistoryDetail")[0]
+                .innerText.split("Condition: ")[1]
+                .split(" Foil").length == 2,
+            "Purchase Price": parseFloat(
+              i
+                .getElementsByClassName("orderHistoryPrice")[0]
+                .innerText.substring(1)
+            ),
+          })
+      ),
     });
-};
+  }
 
+  return orders;
+}
 //filter orders by date range
 
 //need to change full edition name for set code https://moxfield.com/sets
 
+let finalorders = [];
+finalorders.push(scrapeOrders());
 
-
-console.log("ORDERS:")
-console.log(orders);
+console.log("ORDERS:");
+console.log(finalorders);
